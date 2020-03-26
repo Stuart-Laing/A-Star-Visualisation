771a8eebefb3bb4b949555ad8eb590d5f0d7f119
Stuart-Laing <stuart0laing@gmail.com>
2020-03-26 11:19:27 +0000

    Added random start and end points

--- maze_solver/__main__.py
+++ maze_solver/__main__.py
@@ -3,6 +3,7 @@ from maze_solver.draw import draw_border, draw_start_finish, draw_path, draw_nod
 from maze_solver.solve_maze import solve_maze
 
 import turtle
+import random
 
 
 turtle_object = turtle.Turtle()
@@ -26,8 +27,26 @@ for row in range(0, 24):
     for col in range(0, 50):
         draw_node(turtle_object, col, row, maze_array[row][col])
 
-start = (1, 2)
-finish = (22, 20)
+start = (0, 0)
+finish = (0, 0)
+
+clean = False
+
+while start == finish or not clean:
+    start = (random.randint(0, 23), random.randint(0, 49))
+    finish = (random.randint(0, 23), random.randint(0, 49))
+
+    clean = True
+    if maze_array[start[0]][start[1]] == 1:
+        clean = False
+
+    elif maze_array[finish[0]][finish[1]] == 1:
+        clean = False
+
+start = (15, 40)
+finish = (19, 2)
+
+print(start, finish)
 
 draw_start_finish(turtle_object, start[0], start[1], True)
 draw_start_finish(turtle_object, finish[0], finish[1], False)
